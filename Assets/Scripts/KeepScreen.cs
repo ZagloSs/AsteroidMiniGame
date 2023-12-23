b018a28b57a7759de9552471b77527d19d088749
ZagloSs <carlosdiezmendez@gmail.com>
2023-12-23 22:19:45 +0100

    anyadido movimiento de mantener en pantalla

---
+++ Assets/Scripts/KeepScreen.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepScreen : MonoBehaviour
+{
+
+    [SerializeField] private float valorEmpuje;
+    private Camera camaraPrincipal;
+
+    private void Start()
+    {
+        camaraPrincipal = Camera.main;
+    }
+
+    private void Update()
+    {
+
+        Vector3 posicionViewport = camaraPrincipal.WorldToViewportPoint(transform.position);
+
+        if (posicionViewport.x > 1)
+        {
+            transform.position = camaraPrincipal.ViewportToWorldPoint(new Vector3(valorEmpuje, posicionViewport.y, posicionViewport.z));
+        }
+        else if (posicionViewport.x < 0)
+        {
+            transform.position = camaraPrincipal.ViewportToWorldPoint(new Vector3(1- valorEmpuje, posicionViewport.y, posicionViewport.z));
+        }
+
+
+        if (posicionViewport.y > 1)
+        {
+            transform.position = camaraPrincipal.ViewportToWorldPoint(new Vector3(posicionViewport.x, valorEmpuje, posicionViewport.z));
+        }
+        else if (posicionViewport.y < 0)
+        {
+            transform.position = camaraPrincipal.ViewportToWorldPoint(new Vector3(posicionViewport.x, 1-valorEmpuje, posicionViewport.z));
+        }
+
+
+    }
+}
